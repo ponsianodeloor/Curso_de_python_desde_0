0e01b158e2bbaeec480c88dd0f1e0c17c89e41e8
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-01-11 23:37:33 -0500

    agregar elementos unir listas eliminar listas consultar indices

--- 003_listas/lista.py
+++ 003_listas/lista.py
@@ -10,3 +10,30 @@ print(elementos[3:])
 print(elementos[:2])
 
 print(elementos[:3])
+
+#agregar elementos a la lista
+elementos.append('Ponwick')
+print(elementos[:])
+
+#insertar la posicion en el indice para agregar al elemento
+elementos.insert(2, 'Thomas')
+print(elementos)
+
+#agregar mas de un elemento a la lista
+elementos.extend(['Elemento adicional', 'Segundo Item adicional', 'ultimo elemento adicional'])
+print(elementos)
+
+print(elementos.index('Ponwick'))
+
+#consultar si el elemento existe en la lista
+print("Ponwick" in elementos)
+
+#elemento con multiples tipos de datos
+elemento_multiple = ['Ponsiano', 5, 5.60, True]
+elemento_multiple.remove(5)
+elemento_multiple.pop()
+print(elemento_multiple)
+
+#unir listas
+unir_listas= elementos + elemento_multiple
+print(unir_listas)
