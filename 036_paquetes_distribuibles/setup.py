216bfa308e7ca29664d3c589d2fc5737a6aaeb2b
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-01-24 15:34:20 -0500

    paquetes distribuibles
    comando python setup.py sdist para crear paquetes distribuibles y usar las libreras de python dentro del equipo donde se requiera

---
+++ 036_paquetes_distribuibles/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="calculos",
+    version="1.0",
+    description="Paquete de operaciones matematicas basicas",
+    author="ponsianodeloor",
+    author_email="ponsianodeloor@gmail.com",
+    url="https://ponsianodeloor.com/py/package/calculos_matematicos",
+    packages=["calculos"]
+)
