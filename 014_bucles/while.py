8145c2717907d6f603b14e4bb4189c7cbcb7a849
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-05-22 09:35:43 -0500

    edad en while

--- 014_bucles/while.py
+++ 014_bucles/while.py
@@ -3,12 +3,14 @@ while i < 10:
     print(" Ejecucion")
     i += 1
 
-#validar la edad de una persona
+# validar la edad de una persona
 edad = int(input("Ingrese la edad de una persona"))
 
-while edad<0 or edad:
+while edad == 0:
     print("Has introducido una edad negativa. vuelve a intentarlo")
-    input("Ingresa la edad de una persona ")
+    input("Ingresa la edad de una persona: ")
+    edad -= 1
+    print(f"La edad se va reduciendo {edad}")
 
 print("gracias por colaborar, puedes pasar")
 print("Edad " + str(edad))
